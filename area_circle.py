a4fb6e6f3c9b71f62f42986d35bb4eb5ad9af035
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-10 10:53:34 +0530

    Update area_circle.py

--- area_circle.py
+++ area_circle.py
@@ -1,2 +1,2 @@
 a=float(input("Enter The Lenghth Of The Radius :\n"))
-print(f"the area of circle with radius {a} is ", 3.14*a*a)
+print(f"the area of circle with radius {a} is ", 3.141592653589*a*a)
