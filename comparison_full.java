06ff0f78bb52cfde6852fc016340680a68eb9799
Aagam jain <80820614+Aagamj997@users.noreply.github.com>
2022-07-14 22:23:36 +0530

    Add files via upload

---
+++ comparison_full.java
@@ -0,0 +1,10 @@
+public class comparison_full{
+    public static void main (int a, int b){
+        if (a>b)
+        System.out.print(a);
+        else if (a<b)
+        System.out.print(b);
+        else if (a==b)
+        System.out.print("Both Are Equal");
+         }
+}
