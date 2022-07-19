8ed802de4ea4eec3d59ed0d0f352f587ae7e32f5
Namya48 <108971956+Namya48@users.noreply.github.com>
2022-07-19 14:35:41 +0530

    Update perimeter_of_rectangle.java

--- perimeter_of_rectangle.java
+++ perimeter_of_rectangle.java
@@ -1,4 +1,4 @@
-public class area_of_rectangle
+public class perimeter_of_rectangle
 {
     public static void main(double l, double b){
         System.out.print (" Perimeter of Rectangle is " + 2*(l+b));
